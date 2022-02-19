91944db48f16c954d1170612db8a39dde307f6c5
Mickael OLIVIER <molivier@ncelrnd2594.nce.amadeus.net>
2022-02-19 19:54:27 +0100

    Adding MML2VGM folder, MIDI drum map helper image and changes to MOONFARSIDE.MML

---
+++ MML2VGM/removeZoneIdent.bat
@@ -0,0 +1,13 @@
+@echo off
+echo zip,dll,exeファイルのZone識別子を削除します。
+pause
+
+echo on
+FOR %%a in (*.zip *.dll *.exe) do (echo . > %%a:Zone.Identifier)
+FOR %%a in (mml2vgm\*.zip mml2vgm\*.dll mml2vgm\*.exe) do (echo . > %%a:Zone.Identifier)
+FOR %%a in (mml2vgmIDE\*.zip mml2vgmIDE\*.dll mml2vgmIDE\*.exe) do (echo . > %%a:Zone.Identifier)
+@echo off
+
+echo 完了しました。
+pause
+echo on
